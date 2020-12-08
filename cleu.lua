61ec05e18b14ee6bc97d2b1cca9802fb9e1e7a12
Inkounter <inkounter@gmail.com>
2020-12-07 22:14:05 -0500

    Add additional filtering option for 'cleu.lua'.

--- cleu.lua
+++ cleu.lua
@@ -1,13 +1,32 @@
--- trigger: CLEU
+-- trigger (status): CLEU, GROUP_ROSTER_UPDATE
 function(event, ...)
-    if aura_env.config.filterForPlayer then
-        local sourceGuid = select(4, ...)
-        local targetGuid = select(8, ...)
-        local playerGuid = UnitGUID("player")
-        if playerGuid ~= targetGuid and playerGuid ~= sourceGuid then
-            return
+    if event == "GROUP_ROSTER_UPDATE" then
+        local groupUnitGuids = {}
+        aura_env.groupUnitGuids = groupUnitGuids
+        for unit in WA_IterateGroupMembers() do
+            groupUnitGuids[UnitGUID(unit)] = true
         end
-    end
+    else
+        if aura_env.config.unitFilter == 2 then
+            local sourceGuid = select(4, ...)
+            local targetGuid = select(8, ...)
+            local playerGuid = UnitGUID("player")
+            if playerGuid ~= targetGuid and playerGuid ~= sourceGuid then
+                return
+            end
+        elseif aura_env.config.unitFilter == 3 then
+            local sourceGuid = select(4, ...)
+            local targetGuid = select(8, ...)
 
-    print(CombatLogGetCurrentEventInfo())
+            local groupUnitGuids = aura_env.groupUnitGuids
+
+            if groupUnitGuids == nil
+            or (groupUnitGuids[sourceGuid] == nil
+                and groupUnitGuids[targetGuid] == nil) then
+                return
+            end
+        end
+
+        print(CombatLogGetCurrentEventInfo())
+    end
 end
