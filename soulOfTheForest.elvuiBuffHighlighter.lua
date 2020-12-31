1d55cb25c5f5003ee44e5c48dec22dd096aa11f8
Inkounter <inkounter@gmail.com>
2020-12-31 00:06:31 -0500

    Refactor Soul of the Forest logic for object orientation.

--- soulOfTheForest.elvuiBuffHighlighter.lua
+++ soulOfTheForest.elvuiBuffHighlighter.lua
@@ -1,201 +1,248 @@
 -------------------------------------------------------------------------------
 -- init
 
-aura_env.sotfState = {
-    ["ApplySotf"] = function(self)
-        -- Apply the SOTF buff.
-    end,
-
-    ["RemoveSotf"] = function(self, timestamp)
-        -- Remove the SOTF buff at the specified 'timestamp'.
-    end,
-
-    ["Flourish"] = function(self, allstates)
-        -- Refresh the duration and expiration time on all states in the
-        -- specified 'allstates'.
-    end,
-
-    ["ApplyHeal"] = function(self, allstates)
-        -- Apply or refresh a HOT effect.  Update the specified 'allstates' as
-        -- appropriate.
-    end,
-
-    ["RemoveHeal"] = function(self, allstates)
-        -- Remove a HOT effect.  Update the specified 'allstates' as
-        -- appropriate.
-    end
-}
-
-aura_env.sotfApplied = false
-aura_env.empoweredSpell = nil
-aura_env.empoweredCastTime = nil
-aura_env.ignoreNext = {}
-
 aura_env.trackedSpellIds = {
     [114108] = true,    -- Soul of the Forest
+    [197721] = true,    -- Flourish
     [774]    = true,    -- Rejuvenation
     [8936]   = true,    -- Regrowth
     [48438]  = true,    -- Wild Growth
     [155777] = true,    -- Rejuvenation (Germination)
-    [197721] = true,    -- Flourish
 }
 
-aura_env.getGroupUnitId = function(unitGuid)
-    -- Return a unit ID for the specified 'unitGuid' if the unit is in the
-    -- current player's group.  Otherwise, return 'nil'.
+aura_env.sotfState = {
+    ---------------
+    -- PRIVATE DATA
+    ---------------
 
-    for unit in WA_IterateGroupMembers() do
-        if UnitGUID(unit) == unitGuid then
-            return unit
+    ["__sotfApplied"] = false,
+    ["__empoweredTimestamp"] = nil,
+    ["__empoweredSpellId"] = nil,
+
+    ------------------------
+    -- PRIVATE CLASS METHODS
+    ------------------------
+
+    ["__MakeStateKey"] = function(targetGuid, spellId)
+        return spellId .. targetGuid
+    end,
+
+    ["__GetGroupUnitIdFromUnitGuid"] = function(unitGuid)
+        -- Return a unit ID for the unit with the specified 'unitGuid' if the
+        -- unit is in the current player's group.  Otherwise, return 'nil'.
+
+        for unit in WA_IterateGroupMembers() do
+            if UnitGUID(unit) == unitGuid then
+                return unit
+            end
         end
-    end
 
-    return nil
-end
+        return nil
+    end,
 
--------------------------------------------------------------------------------
--- TSU: CLEU:SPELL_AURA_APPLIED:SPELL_AURA_REFRESH:SPELL_AURA_REMOVED:SPELL_CAST_SUCCESS
+    -----------------------
+    -- PRIVATE MANIPULATORS
+    -----------------------
 
-function(allstates, event)
-    local timestamp, subevent, _, sourceGuid, _, _, _, destGuid, _, destFlags, _, spellId = CombatLogGetCurrentEventInfo()
+    ["__EmpowerHeal"] = function(self, timestamp, spellId)
+        -- Try to empower an application of the specified 'spellId' at the
+        -- specified 'timestamp'.  Return 'true' if the application is
+        -- empowered.  Otherwise, return 'false'.
 
-    if sourceGuid ~= UnitGUID("player") then
-        return false
-    end
+        if not self.__sotfApplied
+        and (self.__empoweredTimestamp ~= timestamp or self.__empoweredSpellId ~= nil) then
+            return false
+        end
 
-    if aura_env.trackedSpellIds[spellId] == nil then
-        return false
-    end
+        -- The SOTF aura is applied, or it was removed at 'timestamp' without
+        -- first empowering a spell.  This 'spellId' at 'timestamp' is
+        -- empowered.
 
-    if bit.band(destFlags, COMBATLOG_OBJECT_TYPE_PLAYER) == 0
-    or bit.band(destFlags, COMBATLOG_OBJECT_AFFILIATION_OUTSIDER) ~= 0 then
-        return false
-    end
+        self.__sotfApplied = false
 
-    local destUnit = aura_env.getGroupUnitId(destGuid)
-    if destUnit == nil then
-        return false
-    end
+        if spellId == 48438 then
+            -- This is an application of Wild Growth.  Any following
+            -- applications of Wild Growth at 'timestamp' will also be
+            -- empowered.
 
-    -- By this point, we're sure that this aura is applied by the player, that
-    -- the target is in the player's group, and that the aura is Soul of the
-    -- Forest or a HoT that may be empowered by Soul of the Forest.
+            self.__empoweredTimestamp = timestamp
+            self.__empoweredSpellId = spellId
+        else
+            -- This is an application of another heal.  Empower this
+            -- application, but prevent all following spells from being
+            -- empowered, even if they are at 'timestamp', until the next
+            -- application of the SOTF buff.
 
-    local stateKey = spellId .. destGuid
+            self.__empoweredTimestamp = nil
+            self.__empoweredSpellId = nil
+        end
 
-    if subevent == "SPELL_AURA_APPLIED" and spellId == 197721 then
-        -- The player used Flourish.  Find all shown states in 'allstates' and
-        -- update their durations and expiration times.
+        return true
+    end,
+
+    ---------------
+    -- MANIPULATORS
+    ---------------
+
+    ["ApplySotf"] = function(self)
+        -- Apply the SOTF buff.
+
+        self.__sotfApplied = true
+    end,
+
+    ["RemoveSotf"] = function(self, timestamp)
+        -- Remove the SOTF buff at the specified 'timestamp'.
+
+        self.__sotfApplied = false
+
+        if self.__empoweredTimestamp == timestamp then
+            -- The SOTF buff has already been consumed at 'timestamp' for a
+            -- particular heal.  Do nothing.
+
+            return
+        end
+
+        self.__empoweredTimestamp = timestamp
+        self.__empoweredSpellId = nil
+    end,
+
+    ["Flourish"] = function(self, allstates)
+        -- Refresh the duration and expiration time on all states in the
+        -- specified 'allstates'.  Return 'true' if 'allstates' is updated.
+        -- Otherwise, return 'false'.
 
         local changed = false
         for _, state in pairs(allstates) do
             if state.show == true then
                 local _, _, _, _, duration, expirationTime = WA_GetUnitBuff(state.unit, state.spellId)
-                if state.duration ~= duration then
-                    state.duration = duration
-                    state.expirationTime = expirationTime
-                    state.changed = true
 
-                    changed = true
-                end
+                state.duration = duration
+                state.expirationTime = expirationTime
+                state.changed = true
+
+                changed = true
             end
         end
 
         return changed
-    elseif subevent == "SPELL_AURA_APPLIED"
-    or subevent == "SPELL_AURA_REFRESH"
-    or subevent == "SPELL_CAST_SUCCESS" then
-        if spellId == 114108 then
-            aura_env.sotfApplied = true
-            return false
-        end
+    end,
 
-        if aura_env.sotfApplied then
-            aura_env.sotfApplied = false
-            aura_env.empoweredSpell = spellId
-            aura_env.empoweredCastTime = timestamp
-        end
+    ["ApplyHeal"] = function(self, allstates, timestamp, targetGuid, spellId)
+        -- Apply or refresh a HOT effect with the specified 'spellId' to the
+        -- unit with the specified 'targetGuid' at the specified 'timestamp'.
+        -- Update the specified 'allstates' as appropriate.  Return 'true' if
+        -- 'allstates' is updated.  Otherwise, return 'false'.
 
+        local stateKey = self.__MakeStateKey(targetGuid, spellId)
         local state = allstates[stateKey]
 
-        if aura_env.ignoreNext.target == destUnit
-        and aura_env.ignoreNext.spellId == spellId then
-            aura_env.ignoreNext.target = nil
-            aura_env.ignoreNext.spellId = nil
-
-            return false
-        elseif aura_env.empoweredSpell == spellId
-        and aura_env.empoweredCastTime == timestamp then
-            if spellId ~= 48438 then
-                -- SOTF empowers only the first application of Rejuvenation,
-                -- Germination, or Regrowth, even if Memory of the Mother Tree
-                -- results in multiple applications at the same time.  Reset
-                -- the empowered state so that the following applications do
-                -- not show as empowered.
-
-                aura_env.empoweredSpell = nil
-                aura_env.empoweredCastTime = nil
-
-                -- Ignore the next 'SPELL_AURA_APPLIED', 'SPELL_AURA_REFRESH',
-                -- or 'SPELL_CAST_SUCCESS' event for this same spell on the
-                -- same target so that it doesn't overwrite this empowered
-                -- state.
-
-                aura_env.ignoreNext.target = destUnit
-                aura_env.ignoreNext.spellId = spellId
-            end
+        if self:__EmpowerHeal(timestamp, spellId) then
+            -- Insert or update the state in 'allstates'.
 
             if state == nil then
                 state = {}
                 allstates[stateKey] = state
             end
 
+            local targetUnit = self.__GetGroupUnitIdFromUnitGuid(targetGuid)
+
             state.changed = true
             state.show = true
             state.icon = select(3, GetSpellInfo(spellId))
             state.spellId = spellId
-            state.unit = destUnit
+            state.unit = targetUnit
             state.progressType = "timed"
             state.autoHide = true
 
-            local _, _, _, _, duration, expirationTime = WA_GetUnitBuff(destUnit, spellId)
+            local _, _, _, _, duration, expirationTime = WA_GetUnitBuff(targetUnit, spellId)
             state.duration = duration
             state.expirationTime = expirationTime
 
             return true
-        elseif state ~= nil then
+        else
+            -- Remove the state from 'allstates'.
+
+            if state == nil then
+                return false
+            end
+
             state.changed = true
             state.show = false
 
             return true
-        else
+        end
+    end,
+
+    ["RemoveHeal"] = function(self, allstates, targetGuid, spellId)
+        -- Remove a HOT effect with the specified 'spellId' from the unit with
+        -- the specified 'targetGuid'.  Update the specified 'allstates' as
+        -- appropriate.
+
+        local stateKey = self.__MakeStateKey(targetGuid, spellId)
+        local state = allstates[stateKey]
+
+        if state == nil then
             return false
+        end
+
+        state.changed = true
+        state.show = false
+
+        return true
+    end,
+}
+
+-------------------------------------------------------------------------------
+-- TSU: CLEU:SPELL_AURA_APPLIED:SPELL_AURA_REFRESH:SPELL_AURA_REMOVED
+
+function(allstates, event)
+    local timestamp, subevent, _, sourceGuid, _, _, _, targetGuid, _, targetFlags, _, spellId = CombatLogGetCurrentEventInfo()
+
+    -- Ignore events not caused by the player.
+
+    if sourceGuid ~= UnitGUID("player") then
+        return false
+    end
+
+    -- Ignore untracked spell IDs.
+
+    if aura_env.trackedSpellIds[spellId] == nil then
+        return false
+    end
+
+    -- Ignore events not affecting the player or a group member.
+
+    if bit.band(targetFlags, COMBATLOG_OBJECT_TYPE_PLAYER) == 0
+    or bit.band(targetFlags, COMBATLOG_OBJECT_AFFILIATION_OUTSIDER) ~= 0 then
+        return false
+    end
+
+    -- Update state.
+
+    local sotfState = aura_env.sotfState;
+
+    if subevent == "SPELL_AURA_APPLIED"
+    or subevent == "SPELL_AURA_REFRESH" then
+        if spellId == 114108 then       -- Soul of the Forest
+            sotfState:ApplySotf()
+            return false
+        elseif spellId == 197721 then   -- Flourish
+            return sotfState:Flourish()
+        else
+            return sotfState:ApplyHeal(allstates,
+                                       timestamp,
+                                       targetGuid,
+                                       spellId)
         end
     elseif subevent == "SPELL_AURA_REMOVED" then
-        if spellId == 114108 then
-            -- The SOTF aura is removed after 'SPELL_CAST_SUCCESS', so we've
-            -- already set 'aura_env.empoweredSpell' and
-            -- 'aura_env.empoweredCastTime', and it's safe to set
-            -- 'aura_env.sotfApplied' to false.  ('SPELL_AURA_APPLIED' and
-            -- 'SPELL_AURA_REFRESH' sometimes fire before and sometimes fire
-            -- after 'SPELL_AURA_REMOVED'.)
-
-            aura_env.sotfApplied = false
-
+        if spellId == 114108 then       -- Soul of the Forest
+            sotfState:RemoveSotf()
             return false
+        elseif spellId == 197721 then   -- Flourish
+            return false
+        else
+            return sotfState:RemoveHeal(allstates, targetGuid, spellId)
         end
-
-        local state = allstates[stateKey]
-        if state ~= nil then
-            state.changed = true
-            state.show = false
-
-            return true
-        end
-
-        return false
-    else
-        return false
     end
 end
 
