9435e27fe0258fa326cce81abbe29a56d8d7e9be
Inkounter <inkounter@gmail.com>
2021-04-02 01:37:58 -0400

    Add support for custom nicknames for 'keystoneText.lua'.

--- keystoneText.lua
+++ keystoneText.lua
@@ -1,6 +1,14 @@
---[[
-TODO:
-]]
+-- init
+aura_env.customNames = {
+    [375] = aura_env.config.mists,
+    [376] = aura_env.config.wake,
+    [377] = aura_env.config.side,
+    [378] = aura_env.config.halls,
+    [379] = aura_env.config.plague,
+    [380] = aura_env.config.depths,
+    [381] = aura_env.config.spires,
+    [382] = aura_env.config.theater
+}
 
 -- trigger: BAG_UPDATE_DELAYED, CHALLENGE_MODE_START, CHALLENGE_MODE_COMPLETED, WA_DEFERRED_KEYSTONE_CHECK
 function(event)
@@ -22,7 +30,12 @@ end
 
 -- name
 function()
-    return C_ChallengeMode.GetMapUIInfo(aura_env.keystoneMapId) .. " (" .. C_MythicPlus.GetOwnedKeystoneLevel() .. ")"
+    local name = aura_env.customNames[aura_env.keystoneMapId]
+    if name == "" then
+        name = C_ChallengeMode.GetMapUIInfo(aura_env.keystoneMapId)
+    end
+
+    return name .. " (" .. C_MythicPlus.GetOwnedKeystoneLevel() .. ")"
 end
 
 -- icon
