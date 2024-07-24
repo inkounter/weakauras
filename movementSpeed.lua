8d9faddcc20d3e639a553480f78f5654fb3e3229
Inkounter <inkounter@gmail.com>
2024-07-24 02:00:18 -0400

    Add skyriding speed.

--- movementSpeed.lua
+++ movementSpeed.lua
@@ -13,6 +13,9 @@ end
 -- name
 
 function()
-    local speed = GetUnitSpeed("player")
+    local speed = select(3, C_PlayerInfo.GetGlidingInfo())
+    if speed == 0 then
+        speed = GetUnitSpeed("player")
+    end
     return math.floor(speed / 7 * 100 + 0.5)
 end
